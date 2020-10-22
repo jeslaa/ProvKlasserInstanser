529dacf5ce4ffcdf3b3b7e896e61757977edfa7a
jeslaa <60339624+jeslaa@users.noreply.github.com>
2020-10-22 16:27:54 +0200

    prov

---
+++ Prov/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Prov
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur många kunder vill du ha?: ");
+            
+
+            
+        }
+    }
+}
---
+++ Prov/kudn.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+
+namespace Prov
+{
+    public class Kunder
+    {
+        private int money = 0;
+
+        private Random generator = new Random();
+
+        //skapar nya instanser av kunder
+        Kunder k1 = new Kunder();
+        Kunder k2 = new Kunder();
+        Kunder k3 = new Kunder();
+        Kunder k4 = new Kunder();
+        Kunder k5 = new Kunder();
+        Kunder k6 = new Kunder();
+
+       
+
+
+
+
+    }
+}
